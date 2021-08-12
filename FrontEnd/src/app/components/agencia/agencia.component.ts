c6c1c6b2d6625d128d3c36fef9efaf167d057a8d
Maria <mariayepezmendoza@gmail.com>
2021-08-12 14:51:25 -0500

    update frontend

--- FrontEnd/src/app/components/agencia/agencia.component.ts
+++ FrontEnd/src/app/components/agencia/agencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agencia',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgenciaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  actualizar(){
+    this.router.navigate(['admin']);
   }
 
 }
