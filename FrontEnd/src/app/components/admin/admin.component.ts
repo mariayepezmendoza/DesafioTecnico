021dddde26104f010849ac29e69a9b9f5798dd55
Maria <mariayepezmendoza@gmail.com>
2021-08-12 12:22:26 -0500

    img

--- FrontEnd/src/app/components/admin/admin.component.ts
+++ FrontEnd/src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  public listAgencias:any = []
 
-  constructor() { }
+  constructor(
+    private restService:RestService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarData();
+  }
+
+  public cargarData(){
+    this.restService.get(`https://my-json-server.typicode.com/mariayepezmendoza/DesafioTecnico/agencias`)
+    .subscribe(respuesta =>{
+      console.log(respuesta);
+      this.listAgencias = respuesta;
+    });
   }
 
 }
